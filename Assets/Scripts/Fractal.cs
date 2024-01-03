205022527d0b1cba90368ffbfbfcd39e661dde08
Anton <itonyemail@gmail.com>
2024-01-02 21:15:17 -0800

    making more children going up

--- Assets/Scripts/Fractal.cs
+++ Assets/Scripts/Fractal.cs
@@ -17,5 +17,11 @@ public class Fractal : MonoBehaviour{
         child.transform.SetParent(transform, false);
         child.transform.localPosition = 0.75f * Vector3.right;
         child.transform.localScale = 0.5f * Vector3.one;
+
+        child = Instantiate(this);
+        child.depth = depth - 1;
+        child.transform.SetParent(transform, false);
+        child.transform.localPosition = 0.75f * Vector3.up;
+        child.transform.localScale = 0.5f * Vector3.one;
     }
 }
