60017f6fe3ad706f1352691b15e70b5b70a5792f
Anton <itonyemail@gmail.com>
2024-01-05 20:56:06 -0800

    typo

--- Assets/Scripts/Fractal.cs
+++ Assets/Scripts/Fractal.cs
@@ -4,7 +4,7 @@ using Unity.Jobs;
 using Unity.Mathematics;
 using UnityEngine;
 
-using state Unity.Mathematics.math;
+using static Unity.Mathematics.math;
 using quaternion = Unity.Mathematics.quaternion;
 
 public class Fractal : MonoBehaviour{
