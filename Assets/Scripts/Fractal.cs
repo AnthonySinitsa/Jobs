0a8ee76d4806ac45f6759427eba176a2883f3a52
Anton <itonyemail@gmail.com>
2024-01-05 20:34:54 -0800

    burst compilation

--- Assets/Scripts/Fractal.cs
+++ Assets/Scripts/Fractal.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class Fractal : MonoBehaviour{
 
+    [BurstCompile(CompileSynchronously = true)]
     struct UpdateFractalLevelJob : IJobFor{
 
         public float spinAngleDelta;
