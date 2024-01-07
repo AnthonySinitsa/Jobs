023f65b2978a0b744a4fa7828369242c9e4749e9
Anton <itonyemail@gmail.com>
2024-01-07 13:22:34 -0800

    interloping colors

--- Assets/Scripts/Fractal.cs
+++ Assets/Scripts/Fractal.cs
@@ -126,7 +126,9 @@ public class Fractal : MonoBehaviour{
             ComputeBuffer buffer = matricesBuffers[i];
             buffer.SetData(matrices[i]);
             propertyBlock.SetColor(
-				baseColorId, Color.white * (i / (matricesBuffers.Length - 1f))
+				baseColorId, Color.Lerp(
+                    Color.yellow, Color.red, i / (matricesBuffers.Length - 1f)
+                )
 			);
             propertyBlock.SetBuffer(matricesId, buffer);
             Graphics.DrawMeshInstancedProcedural(
